18a9149bdd5a3b3484c128e94b363a9f7542cb8d
= <=>
2022-12-13 19:32:01 +0700

    Lab3_complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,27 @@
 
 using namespace std;
 
+int adiff(int x, int y) {
+    int a = x%360;
+    int b = y%360;
+    int c = a - b;
+    if(c < 0) {
+        c = c*(-1); 
+    } 
+    if(c > 180) {
+        c = 360 -c;
+    }
+    return c;
+}
+
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;
 }
